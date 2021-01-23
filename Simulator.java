faa2db6f28247a29ee54ed9ff496721c8caee67b
KenOafallasDLSU <kenneth_oafallas@dlsu.edu.ph>
2021-01-23 10:36:46 +0800

    Setters/Getters, initial movement logic

--- Simulator.java
+++ Simulator.java
@@ -1,4 +1,6 @@
 /**
+ * Simulator handles cache movement simulations and miss/hit counting
+ * 
  * @author S12 Group 3
  * @author BUHION, Deborah Rose P.
  * @author DIZON, Michaela Nicole P.
@@ -6,49 +8,123 @@
  * @author OAFALLAS, Kenneth Neil B.
  */
 
- /*
-simulation is a module with the functions
-
-constructor Simulator(cacheAccessTime, mmAccessTime, programFlow, cacheBlocks, mmBlocks, blockSize)
-array getCacheSnapsot()
-int getMissCount()
-int getHitCount()
-*/
-
-import java.io.File;
+import java.util.ArrayList;
 
 public class Simulator{
-  private float cacheAccessTime;
-  private float mmAccessTime;
+  //input
   private String programFlow;
   private int cacheBlocks;
   private int mmBlocks;
   private int blockSize;
-  
+
+  //output
+  private int[][] cacheMemory;
+  private int[][] mainMemory;
+  private int missCount;
+  private int hitCount;
+
   public Simulator() {
-    /*
-    this.cacheAccessTime=cacheAccessTime;
-    this.mmAccessTime=mmAccessTime;
-    //this.programFlow=Arrays.copyOf(programFlow, programFlow.length);
-    this.programFlow= new String(programFlow);
-    this.cacheBlocks=cacheBlocks;
-    this.mmBlocks=mmBlocks;
-    this.blockSize=blockSize;
-    */
+    this.programFlow = "";
+    this.cacheBlocks = 0;
+    this.mmBlocks = 0;
+    this.blockSize = 0;
+
+    this.cacheMemory = new int[0][0];
+    this.mainMemory = new int[0][0];
+    this.missCount = 0;
+    this.hitCount = 0;
   }
 
-  public void setProgramFlow(String programFlow){
-    this.programFlow=programFlow;
+  /**
+   * runSimulation sets the output values based on the current data in input values
+   * 
+   * @precondition - input values must be set beforehand
+   */
+  public void runSimulation() {
+    this.cacheMemory = new int[this.cacheBlocks][this.blockSize];
+    this.mainMemory = initMainMemory();
+    this.missCount = 0;
+    this.hitCount = 0;
+
+    ArrayList<Integer> parsedFlow = parseProgramFlow();
+
+    for(int i=0; i < parsedFlow.size(); i++){
+      boolean isHit = true;
+      int mainBlkAddress = parsedFlow.get(i);
+      int cacheBlkAddress = mainBlkAddress % cacheBlocks;
+      
+      for(int k=0; k < blockSize; k++) {
+        if(this.cacheMemory[cacheBlkAddress][k] != this.mainMemory[mainBlkAddress][k])
+        {
+          isHit = false;
+          break;
+        }
+      }
+
+      if(isHit) {
+        this.hitCount++;
+      } else{
+        this.missCount++;
+        this.cacheMemory[cacheBlkAddress] = this.mainMemory[mainBlkAddress];
+      }
+    }
   }
 
-  public void printWords(){
-    System.out.println(this.programFlow);
+  /**
+   * Initializes addresses in main memory
+   */
+  private int[][] initMainMemory() {
+    int[][] filledMemory = new int[this.mmBlocks][this.blockSize];
+    int mmCtr = 0;
+
+    for(int i=0; i < this.mmBlocks; i++){
+      for(int k=0; k < this.blockSize; k++){
+        filledMemory[i][k] = mmCtr;
+        mmCtr++;
+      }
+    }
+
+    return filledMemory;
   }
 
-  public void setMMBlocks(int mmBlocks){
-    this.mmBlocks=mmBlocks;
+  /**
+   * Parses program flow into sequence of ints representing MM addresses
+   */
+  private ArrayList<Integer> parseProgramFlow() {
+    ArrayList<Integer> parsedFlow = new ArrayList<Integer>();
+
+    //logic here
+    parsedFlow.add(23) //sample data
+    parsedFlow.add(12)
+    parsedFlow.add(9)
+    parsedFlow.add(9)
+
+    return parsedFlow;
   }
 
+
+  //setters
+  public void setProgramFlow(String programFlow) {
+    this.programFlow = programFlow;
+  }
+  public void setCacheBlocks(int cacheBlocks) {
+    this.cacheBlocks = cacheBlocks;
+  }
+  public void setMMBlocks(int mmBlocks) {
+    this.mmBlocks = mmBlocks;
+  }
+  public void setBlockSize(int blockSize) {
+    this.blockSize = blockSize;
+  }
+
+  //getters
+  public String[][] getCacheSnapshot() {
+    return this.cacheMemory;
+  }
+  public int getMissCount() {
+    return this.missCount;
+  }
+  public int getHitCount() {
+    return this.hitCount;
+  }
 }
-
-
