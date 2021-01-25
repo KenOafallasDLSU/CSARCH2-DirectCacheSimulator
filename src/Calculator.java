bc80f94a43f0eff4873b831e620a406c76686fd2
drbuhion <debbie@buhion.com>
2021-01-25 17:02:18 +0800

    Calculator skeleton code

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,5 +7,36 @@
  */
 
 public class Calculator {
-  ;
+  public Calculator (int cacheAccessTime, int mmAccessTime, int hitCount, int missCount){
+    
+    this.cacheAccessTime = cacheAccessTime;
+    this.mmAccessTime = mmAccessTime;
+    this.hitCount = hitCount;
+    this.missCount = missCount;
+
+  }
+
+  public float getMissPenalty(){
+      float missPenalty = 0;
+
+      return missPenalty;
+  }
+
+  public float getAverageTime(){
+    float averageTime = 0;
+
+    return averageTime;
+  }
+
+  public float getTotalTime(){
+    float totalTime = 0;
+
+    return totalTime;
+  }
+
+
+  private int cacheAccessTime;
+  private int mmAccessTime;
+  private int hitCount;
+  private int missCount;
 }
