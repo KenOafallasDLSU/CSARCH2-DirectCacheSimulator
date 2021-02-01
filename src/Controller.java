7f1bd28539a8c2e5e5db083de2b80484d47921e7
James Kevin Lin <lin.james0825@yahoo.com>
2021-02-01 17:51:58 +0800

    Add pop up messages when error

--- src/Controller.java
+++ src/Controller.java
@@ -13,6 +13,7 @@ import java.io.FileWriter;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import javax.swing.JFileChooser;
+import static javax.swing.JOptionPane.showMessageDialog;
 public class Controller {
 
     private View view;
@@ -43,18 +44,23 @@ public class Controller {
           model.setCacheAccessTime(Float.parseFloat(view.cat.getText()));
           model.setMMAccessTime(Float.parseFloat(view.mat.getText()));
         }catch(NumberFormatException exception){
+          
+          showMessageDialog(null, "Number in cache access time and memory access time please");
           System.out.println("Number in cache access time and memory access time please");
         }
         catch(Exception except){
+          showMessageDialog(null, "Unknown error occured");
           except.printStackTrace();
         }
 
         try{
           model.setBlockSize(Integer.parseInt(view.blockSize.getText()));
         }catch(NumberFormatException exception){
+          showMessageDialog(null, "int in block size please");
           System.out.println("int in block size please");
         }
         catch(Exception except){
+          showMessageDialog(null, "Unknown error occured");
           except.printStackTrace();
         }
         
@@ -79,12 +85,15 @@ public class Controller {
           }
         }
         catch(NumberFormatException except){
+          showMessageDialog(null, "Number in cache size and memory size please");
           System.out.println("Number in cache size and memory size please");
         }
         catch(ArithmeticException except){
+          showMessageDialog(null, "Make sure the block size is not zero or blank please");
           System.out.println("Make sure the block size is not zero or blank please");
         }
         catch(Exception except){
+          showMessageDialog(null, "Unknown error occured");
           except.printStackTrace();
         }
         
@@ -109,9 +118,11 @@ public class Controller {
         try{
           model.setCalculator();
         }catch(ArithmeticException except){
+          showMessageDialog(null, "Make sure the block size is not zero or blank please");
           System.out.println("Make sure the block size is not zero or blank please");
         }
         catch(Exception except){
+          showMessageDialog(null, "Unknown error occured in calculating");
           except.printStackTrace();
         }
         
@@ -167,30 +178,35 @@ public class Controller {
 
           JFileChooser f = new JFileChooser();
           f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
-          f.showSaveDialog(null);
-          System.out.println("my file is: "+f.getSelectedFile());
+          int result=f.showSaveDialog(null);
+          
+          if(result == JFileChooser.APPROVE_OPTION){
+            System.out.println("my file is: "+f.getSelectedFile());
 
-          String filename=f.getSelectedFile()+"\\"+"Result_Log_"+sdf.format(timestamp)+".txt";
-          System.out.println("file is: "+filename);
+            String filename=f.getSelectedFile()+"\\"+"Result_Log_"+sdf.format(timestamp)+".txt";
+            System.out.println("file is: "+filename);
 
-          File myObj = new File(filename);
-          if (myObj.createNewFile()) {
-            System.out.println("File created: " + myObj.getName());
-          } else {
-            System.out.println("File already exists.");
+            File myObj = new File(filename);
+            if (myObj.createNewFile()) {
+              System.out.println("File created: " + myObj.getName());
+            } else {
+              System.out.println("File already exists.");
+            }
+            FileWriter myWriter = new FileWriter(filename);
+            String toBeWritten=
+              "Miss count: "+model.getMissCount()+System.lineSeparator()+
+              "Hit count: "+model.getHitCount()+System.lineSeparator()+
+              "Miss penalty: "+model.getMissPenalty()+" ns"+System.lineSeparator()+
+              "Average time: "+model.getAverageTime()+" ns"+System.lineSeparator()+
+              "Total time: "+model.getTotalTime()+" ns"+System.lineSeparator()+System.lineSeparator()+
+              model.getCacheSnapshot();
+            myWriter.write(toBeWritten);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
           }
-          FileWriter myWriter = new FileWriter(filename);
-          String toBeWritten=
-            "Miss count: "+model.getMissCount()+"\n"+
-            "Hit count: "+model.getHitCount()+"\n"+
-            "Miss penalty: "+model.getMissPenalty()+" ns\n"+
-            "Average time: "+model.getAverageTime()+" ns\n"+
-            "Total time: "+model.getTotalTime()+" ns\n"+"\n"+
-            model.getCacheSnapshot();
-          myWriter.write(toBeWritten);
-          myWriter.close();
-          System.out.println("Successfully wrote to the file.");
+             
         } catch (IOException error) {
+          showMessageDialog(null, "Unknown error occured in file exporting");
           System.out.println("An error occurred.");
           error.printStackTrace();
         }
